6cdc4301410508454417f6992634a0871ec0545c
mark182182 <versalife11@hotmail.com>
2019-02-23 11:16:36 +0100

    fix(/message): add db.commit to make changes to the database

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 import MySQLdb as MySQL
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def get_index():
@@ -11,20 +12,25 @@ def get_index():
 
 @app.route('/chat', methods=['GET'])
 def get_chat_messages():
-  db = MySQL.connect('localhost', 'root', '12345', "chat")
+  db = MySQL.connect('172.17.0.2', 'root', '12345', "chat")
   cur = db.cursor()
   cur.execute('SELECT * FROM messages')
   messages = jsonify(cur.fetchall())
+  cur.close()
+  db.close()
   return messages
 
 
 @app.route('/message', methods=['POST'])
 def get_current_message_and_save_to_database():
   content = request.json
-  db = MySQL.connect('localhost', 'root', '12345', "chat")
+  db = MySQL.connect('172.17.0.2', 'root', '12345', "chat")
   cur = db.cursor()
   cur.execute('INSERT INTO messages (username, message) VALUES(%s, %s)',
               (content['username'], content['message']))
+  db.commit()
+  cur.close()
+  db.close()
   return jsonify(content)
 
 
