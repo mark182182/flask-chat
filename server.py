cb61a196f9772ae941a593dcb7b581baf08a32ab
Vidics <vidics.mark@zlt.hu>
2019-03-01 13:55:13 +0100

    feat(socketio): send back the current message via ws

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, send_from_directory, jsonify, request
 from flask_cors import CORS
+from flask_socketio import SocketIO
+import os
 import MySQLdb as MySQL
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates')
+app.config['SECRET_KEY'] = 'karton'
+socketio = SocketIO(app)
 CORS(app)
 
-@app.route('/')
-def get_index():
-  return render_template('index.html')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+  if path != "" and os.path.exists("react_app/build/" + path):
+    return send_from_directory('react_app/build', path)
+  else:
+    return send_from_directory('react_app/build', 'index.html')
 
 
 @app.route('/chat', methods=['GET'])
 def get_chat_messages():
-  db = MySQL.connect('172.17.0.2', 'root', '12345', "chat")
+  db = MySQL.connect('localhost', 'root', '12345', 'chat', charset='utf8')
   cur = db.cursor()
   cur.execute('SELECT * FROM messages')
   messages = jsonify(cur.fetchall())
@@ -24,14 +33,23 @@ def get_chat_messages():
 @app.route('/message', methods=['POST'])
 def get_current_message_and_save_to_database():
   content = request.json
-  db = MySQL.connect('172.17.0.2', 'root', '12345', "chat")
+  db = MySQL.connect('localhost', 'root', '12345', 'chat', charset='utf8')
   cur = db.cursor()
   cur.execute('INSERT INTO messages (username, message) VALUES(%s, %s)',
               (content['username'], content['message']))
+  count_messages = cur.execute('SELECT * FROM messages')
+  if (count_messages == 21):
+    cur.execute('DELETE FROM messages ORDER BY id LIMIT 1')
   db.commit()
   cur.close()
   db.close()
+  socketio.emit('message', content)
   return jsonify(content)
 
 
-app.run(debug=True)
+@socketio.on('connect')
+def connected():
+  print "%s connected" % (request.sid)
+
+
+socketio.run(app, use_reloader=True, host='192.168.111.234')
